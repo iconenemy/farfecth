1f195f8aa0183134926e3bba0400bfa228edbf45
iconenemy <iconenemy@gmail.com>
2023-03-24 22:33:36 +0200

    feat: Connect to db

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModuleAsyncOptions, TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: (configService: ConfigService) => {
+				return {
+					type: configService.get('POSTGRES_TYPE'),
+					host: configService.get('POSTGRES_HOST'),
+					port: configService.get('POSTGRES_PORT'),
+					username: configService.get('POSTGRES_USER'),
+					password: configService.get('POSTGRES_PASSWORD'),
+					database: configService.get('POSTGRES_DATABASE'),
+					entities: [],
+					synchronize: true,
+					keepConnectionAlive: true
+				} as TypeOrmModuleAsyncOptions;
+			}
+		}),
+	],
+	controllers: [],
+	providers: [],
+	exports: []
 })
 export class AppModule {}
